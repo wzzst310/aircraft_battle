4c7d481e84806a1c95b5db8ce7866245eb976ae1
“wzzst310” <wzzst310@163.com>
2018-12-14 00:35:04 +0800

    Initial commit

---
+++ _01_pygame入门.py
@@ -0,0 +1,6 @@
+import pygame
+
+# 编写游戏的代码
+pygame.init()
+print("游戏的代码...")
+pygame.quit()
---
+++ _03_创建游戏窗口.py
@@ -0,0 +1,7 @@
+import pygame
+
+pygame.init()
+pygame.display.set_mode((480,700))
+while True:
+    pass
+
